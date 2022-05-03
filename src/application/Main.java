da884309aaaa5224cd9753386aa754eea14d3635
Lucas Menescal Pontes <lucasmenescalpontes@gmail.com>
2022-05-03 13:08:44 -0300

    Adicionan nova view: About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,14 +9,18 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
+			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Aplicação FX");
 			primaryStage.show();
@@ -25,6 +29,11 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+		
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
